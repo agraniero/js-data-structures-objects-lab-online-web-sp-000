11335ccc0aacc31d15aec0599486f265a489e5bb
Anthony Graniero <ananfro@gmail.com>
2019-12-05 16:36:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
 
 function deleteFromDriverByKey(driver, key){
   const newObject = Object.assign({}, driver);
-  newObject.delete;
+  newObject[key].delete;
   return newObject;
 }
 
